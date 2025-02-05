74a49c7e40a6f564fd9fceecdf7073534a45dd41
Wavezy11 <wavezy.dd@gmail.com>
2025-02-05 11:58:09 +0100

    Updated server.js
    added EJS template to the project

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const PORT = 3010;
-const prettyLog = require('pretty-log');
+// const prettyLog = require('pretty-log');
 
 //
-app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    var mascots = [
+      { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+      { name: 'Tux', organization: "Linux", birth_year: 1996},
+      { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+  
+    res.render('pages/index', {
+      mascots: mascots,
+      tagline: tagline
+    });
+  });
+
+  
+  // about page
+  app.get('/about', function(req, res) {
+    res.render('pages/about');
+  });
+// app.use(express.urlencoded({ extended: false }));
 
 // Handle GET request
 app.get('/', function (req, res) {
